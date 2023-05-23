0b35c7e991b730cf60409c22e8bc25c1f8dbb873
Julian Casallas <julian.casallasb@gmail.com>
2023-05-23 16:31:43 -0500

    genres search

--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from users.models import User
 # Create your models here.
-class Film(models.Model):
+class Like(models.Model):
     id_film = models.IntegerField(blank=False,null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE,null=False)
--- search_movie/views.py
+++ search_movie/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from API_TMDB.views import tmdb_api
-from django.http import HttpResponse
+
 # Create your views here.
 def get_genres(request):
     _request = tmdb_api('genre/movie','list')
     context = {'genres' : _request.json()['genres']}
 
     return render(request,'movies/search_movie.html',context=context)
+
+def get_movies(request):
+    """
+    Renderiza la página principal con una lista de películas.
+    """
+    movies = movies_json(request).content.decode("utf-8")
+    context = {"movies": json.loads(movies)}
+
+    return render(request, "index.html", context)
